52a87b0b4ede71e33b41111273ed5a0e69317923
Huan Tran <huanductran98@gmail.com>
2022-07-10 18:08:19 -0700

    Complete exercise

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-// TODO
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hovered: false
+    };
+  }
+
+  onGroceryItemMouseOver() {
+    this.setState({
+      hovered: !this.state.hovered
+    })
+  }
+
+  render() {
+    var style = {
+      fontWeight: this.state.hovered ? 'bold' : 'normal'
+    };
+
+    return (
+      <li style={style} onMouseOver={this.onGroceryItemMouseOver.bind(this)}>{this.props.item}</li>
+    );
+  }
+}
+
+
+var GroceryList = (props) => (
+  <ul>
+    {props.groceryItems.map(item =>
+      <GroceryListItem item={item} />
+    )}
+  </ul>
+);
+
+
+var App = () => (
+  <div>
+    <h2>My Grocery List</h2>
+    <GroceryList groceryItems={['Cucumbers', 'Kale']}/>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
